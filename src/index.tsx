18298837f354837bad0ad890dffc87ab313b8ef2
Meldin Radoncic <meldinradoncic89@gmail.com>
2025-01-10 09:41:11 -0500

    Adding Clerk Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react'
+
+
+
+const frontendApi = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY
+
+if (!frontendApi) {
+  throw new Error("Missing Publishable Key")
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={frontendApi}>
+      <App />
+    </ClerkProvider>
+    
   </React.StrictMode>
 );
