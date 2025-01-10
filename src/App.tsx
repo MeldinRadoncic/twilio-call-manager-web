ea0bed51b143ccaf943baf66d69408eb98164646
Meldin Radoncic <meldinradoncic89@gmail.com>
2025-01-10 09:40:35 -0500

    Adding Clerk Auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
-import './App.css';
-
-import Call from './components/call/Call';
+import React from "react";
+import "./App.css";
+import {
+  SignedIn,
+  SignIn,
+  SignedOut,
+  RedirectToSignIn,
+  UserButton,
+  useClerk,
+} from "@clerk/clerk-react";
+import Call from "./components/call/Call";
 
 const App: React.FC = () => {
-  return <div className="App">
-    <Call />
-  </div>;
+  const { signOut } = useClerk();
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        {/* If the user is signed in */}
+        <SignedIn>
+          <div className='user'>
+            <button
+              type='submit'
+              className='sign-out-btn'
+              onClick={() => signOut()}>
+              Sign Out
+            </button>
+            <UserButton />
+          </div>
+          <Call />
+          <button
+            onClick={() => signOut()}>
+            Sign Out
+          </button>
+        </SignedIn>
+
+        {/* If the user is signed out, redirect to the sign-in page */}
+        <SignedOut>
+          <RedirectToSignIn />
+        </SignedOut>
+      </header>
+    </div>
+  );
 };
 
 export default App;
